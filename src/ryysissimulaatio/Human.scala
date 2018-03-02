7085c32dd36a1fad09db2594496112f120bba56f
Eretzu <m.hemilae@gmail.com>
2018-03-02 20:09:40 +0200

    changed steering algorithm to class.

--- src/ryysissimulaatio/Human.scala
+++ src/ryysissimulaatio/Human.scala
@@ -4,12 +4,13 @@ class Human( x: Double, y: Double, val room: Room) {
   val mass = 1
   private var _position = Vector2D(x, y)
   private var _velocity = Vector2D(0,0)
+  private val steering = new SteeringAlgorithm(this, room)
   //private var heading = 0
   
   def position = _position
   def velocity = _velocity
   
-  private def getAcceleration() = SteeringAlgorithm.getAcceleration(this, room).derive(0, Human.MaxForce)
+  private def getAcceleration() = steering.getAcceleration().derive(0, Human.MaxForce)
   
   def move() = {
     _velocity += getAcceleration()
--- src/ryysissimulaatio/SteeringAlgorithm.scala
+++ src/ryysissimulaatio/SteeringAlgorithm.scala
@@ -2,24 +2,24 @@ package ryysissimulaatio
 
 import scala.util.Random
 
-object SteeringAlgorithm {
-  def getAcceleration ( human: Human, room: Room ) : Vector2D = {
+class SteeringAlgorithm( val human: Human, val room: Room) {
+  def getAcceleration () : Vector2D = {
     var returnVector = new Vector2D(0,0)
-    returnVector += seek(human, room).truncate(1.0)
-    returnVector += brake(human, room).truncate(10.0)
-    returnVector += avoid(human, room).truncate(1.0)
+    returnVector += seek().truncate(1.0)
+    returnVector += brake().truncate(10.0)
+    returnVector += avoid().truncate(1.0)
     returnVector += wander().truncate(1.0)
     return returnVector.normalize()
   }
   
-  private def seek(human: Human, room: Room) : Vector2D = {
+  private def seek() : Vector2D = {
     val desiredVelocity = (room.getDoorCoordinates - human.position).truncate(Human.MaxSpeed)
     desiredVelocity - human.velocity
   }
   
-  private def brake(human: Human, room: Room) : Vector2D = Vector2D(0,0)
+  private def brake() : Vector2D = Vector2D(0,0)
   
-  private def avoid(human: Human, room: Room) : Vector2D = Vector2D(0,0)
+  private def avoid() : Vector2D = Vector2D(0,0)
   
   private def wander() : Vector2D = {
     val rand = new Random()
