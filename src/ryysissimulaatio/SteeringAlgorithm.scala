f3be185701b39c6192c755ed67c6f87d17820027
Eretzu <m.hemilae@gmail.com>
2018-03-02 19:57:41 +0200

    Implemented seeking the door.

--- src/ryysissimulaatio/SteeringAlgorithm.scala
+++ src/ryysissimulaatio/SteeringAlgorithm.scala
@@ -5,23 +5,24 @@ import scala.util.Random
 object SteeringAlgorithm {
   def getAcceleration ( human: Human, room: Room ) : Vector2D = {
     var returnVector = new Vector2D(0,0)
-    returnVector += seek(human, room)
-    returnVector += brake(human, room)
-    returnVector += avoid(human, room)
-    returnVector += wander()
-    return returnVector
+    returnVector += seek(human, room).truncate(1.0)
+    returnVector += brake(human, room).truncate(10.0)
+    returnVector += avoid(human, room).truncate(1.0)
+    returnVector += wander().truncate(1.0)
+    return returnVector.normalize()
   }
   
   private def seek(human: Human, room: Room) : Vector2D = {
-    return Vector2D(0, 0)
+    val desiredVelocity = (room.getDoorCoordinates - human.position).truncate(Human.MaxSpeed)
+    desiredVelocity - human.velocity
   }
   
-  private def brake(human: Human, room: Room) : Vector2D = return Vector2D(0,0)
+  private def brake(human: Human, room: Room) : Vector2D = Vector2D(0,0)
   
-  private def avoid(human: Human, room: Room) : Vector2D = return Vector2D(0,0)
+  private def avoid(human: Human, room: Room) : Vector2D = Vector2D(0,0)
   
   private def wander() : Vector2D = {
     val rand = new Random()
-    return Vector2D(rand.nextDouble()-rand.nextDouble(), rand.nextDouble()-rand.nextDouble()).normalize
+    Vector2D(rand.nextDouble()-rand.nextDouble(), rand.nextDouble()-rand.nextDouble()).normalize
   }
 }
