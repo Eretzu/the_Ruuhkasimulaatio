08951644930549a44e572879af3b73023c4f7c3a
Eretzu <m.hemilae@gmail.com>
2018-03-02 10:52:27 +0200

    Added wander and started implementing the whole algorithm.

--- src/ryysissimulaatio/SteeringAlgorithm.scala
+++ src/ryysissimulaatio/SteeringAlgorithm.scala
@@ -1,17 +1,27 @@
 package ryysissimulaatio
 
+import scala.util.Random
+
 object SteeringAlgorithm {
-  def getAcceleration ( human: Human, room:Room ) : Vector2D = {
+  def getAcceleration ( human: Human, room: Room ) : Vector2D = {
     var returnVector = new Vector2D(0,0)
-    returnVector = returnVector + seek
-    returnVector = returnVector + brake
-    returnVector = returnVector + avoid
+    returnVector += seek(human, room)
+    returnVector += brake(human, room)
+    returnVector += avoid(human, room)
+    returnVector += wander()
     return returnVector
   }
   
-  private def seek () : Vector2D = ???
+  private def seek(human: Human, room: Room) : Vector2D = {
+    return Vector2D(0, 0)
+  }
   
-  private def brake () : Vector2D = ???
+  private def brake(human: Human, room: Room) : Vector2D = return Vector2D(0,0)
   
-  private def avoid () : Vector2D = ???
+  private def avoid(human: Human, room: Room) : Vector2D = return Vector2D(0,0)
+  
+  private def wander() : Vector2D = {
+    val rand = new Random()
+    return Vector2D(rand.nextDouble()-rand.nextDouble(), rand.nextDouble()-rand.nextDouble()).normalize
+  }
 }
