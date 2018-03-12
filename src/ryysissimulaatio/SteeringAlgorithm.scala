4e413d52993ae30f943a6d6dbd44ca8fec53995a
Eretzu <m.hemilae@gmail.com>
2018-03-12 03:43:02 +0200

    Starting to reimplement steering algorithm.

--- src/ryysissimulaatio/SteeringAlgorithm.scala
+++ src/ryysissimulaatio/SteeringAlgorithm.scala
@@ -6,27 +6,22 @@ import scala.math.pow
 class SteeringAlgorithm( val human: Human, val room: Room) {
   def getAcceleration () : Vector2D = {
     var returnVector = new Vector2D(0,0)
-    returnVector += seek().truncate(1.0)
-    returnVector += avoid().truncate(2.0)
-    returnVector += wander().truncate(1.0)
-    returnVector += separation().truncate(1.0)
+    returnVector += seek()
+    //returnVector += avoid()
+    //returnVector += wander()
+    //returnVector += separation()
     returnVector
   }
   
   private def seek() : Vector2D = {
-    val desiredVelocity = (room.getDoorCoordinates - human.position).truncate(Human.MaxSpeed)
+    val desiredPosition = room.door - human.position
+    if(human.position.x < 0 || desiredPosition.length < room.doorWidth/2) return Vector2D(-10, 0)
+    val desiredVelocity = desiredPosition.truncate(Human.MaxSpeed)
     desiredVelocity - human.velocity
   }
   
   private def avoid() : Vector2D = {
     var x, y = 0
-    val WallDistance = 20
-    if(human.position.x < WallDistance && 
-      (human.position.y < room.door-WallDistance || human.position.y > room.door+WallDistance)) 
-      x = (30 - human.position.x.toInt)/WallDistance*2
-    else if(room.width - human.position.x < WallDistance) x = (WallDistance - (room.width - human.position.x.toInt))/WallDistance*2
-    if(human.position.y < WallDistance) y = (WallDistance - human.position.y.toInt)/WallDistance*2
-    else if(room.width - human.position.y < WallDistance) y = (WallDistance - (room.width - human.position.y.toInt))/WallDistance*2 
     Vector2D(x,y)
   }
    
