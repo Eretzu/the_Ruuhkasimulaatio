11adf8bfdb5997f0f813b8fd56b5cad765e33292
Eretzu <m.hemilae@gmail.com>
2018-03-12 04:14:31 +0200

    Implementing steering. Separation might not be suitable here.

--- src/ryysissimulaatio/SteeringAlgorithm.scala
+++ src/ryysissimulaatio/SteeringAlgorithm.scala
@@ -6,16 +6,16 @@ import scala.math.pow
 class SteeringAlgorithm( val human: Human, val room: Room) {
   def getAcceleration () : Vector2D = {
     var returnVector = new Vector2D(0,0)
-    returnVector += seek()
-    //returnVector += avoid()
-    //returnVector += wander()
-    //returnVector += separation()
+    returnVector += seek()*1
+    //returnVector += avoid()*1
+    //returnVector += wander()*1
+    //returnVector += separation()*100
     returnVector
   }
   
   private def seek() : Vector2D = {
     val desiredPosition = room.door - human.position
-    if(human.position.x < 0 || desiredPosition.length < room.doorWidth/2) return Vector2D(-10, 0)
+    if(human.position.x < 0 || desiredPosition.length < room.doorWidth/2) return Vector2D(-1, 0)
     val desiredVelocity = desiredPosition.truncate(Human.MaxSpeed)
     desiredVelocity - human.velocity
   }
@@ -25,13 +25,12 @@ class SteeringAlgorithm( val human: Human, val room: Room) {
     Vector2D(x,y)
   }
    
-  private def separation() : Vector2D = {
-    val neighborhood = room.humans.map(other => (human.position - other.position)).filter( _.length < 80 )
-    neighborhood.map( distance => distance * pow(distance.length, 2) ).fold(Vector2D(0, 0)) { _ + _ }
-  }
-  
-  // maybe not needed with separation
-  //private def brake: Vector2D = ???
+  /*private def separation() : Vector2D = {
+    val back = -human.velocity
+    val visible = room.humans.filter(other => (other.position angle back) > math.Pi/3)
+    val neighborhood = visible.map(other => (human.position - other.position)).filter( x => x.length < 30 && x.length > 0.01 )
+    neighborhood.map( distance => distance / pow(distance.length, 2) ).fold(Vector2D(0, 0)) { _ + _ }
+  }*/
   
   private def wander() : Vector2D = {
     val rand = new Random()
