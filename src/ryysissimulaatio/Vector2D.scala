795e72ad9ffdb8e37b6bcee17429a157385f22e9
Eretzu <m.hemilae@gmail.com>
2018-04-23 15:24:00 +0300

    A few comments.

--- src/ryysissimulaatio/Vector2D.scala
+++ src/ryysissimulaatio/Vector2D.scala
@@ -14,20 +14,21 @@ case class Vector2D(x: Double, y: Double) {
   
   def unary_- = Vector2D(-x, -y)
   
+  // Returns a unit vector of given vector
   def normalize() = Vector2D(x/length, y/length)
   
+  // Truncates vector to given value
   def truncate(maxLength: Double) = {
     if(this.length > maxLength) Vector2D(x/length*maxLength, y/length*maxLength)
     else this
   }
   
+  // Dot product of 2 vectors
   def dot (other: Vector2D): Double = this.x*other.x + this.y*other.y
   
+  // Angle between 2 vectors
   def angle (other: Vector2D): Double = math.acos((this dot other)/(this.length*other.length))
   
-  /**
-   * Lakee yhteen kaksi vektoria ja palauttaa uuden vektorin
-   */
   def + (other: Vector2D) = {
     Vector2D(x + other.x, y + other.y)    
   }  
